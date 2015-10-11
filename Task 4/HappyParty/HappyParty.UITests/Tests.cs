c67925a36638e404a86314bb8557850994d3b958
Святослав Щавровский <shchaurouski.slava@iMac-slawiko.local>
2015-10-11 04:37:20 +0300

    T-4: Switch direction from Android to iOS

--- Task 4/HappyParty/HappyParty.UITests/Tests.cs
+++ Task 4/HappyParty/HappyParty.UITests/Tests.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Linq;
 using NUnit.Framework;
 using Xamarin.UITest;
-using Xamarin.UITest.Android;
+using Xamarin.UITest.iOS;
 using Xamarin.UITest.Queries;
 
 namespace HappyParty.UITests
@@ -11,26 +11,23 @@ namespace HappyParty.UITests
 	[TestFixture]
 	public class Tests
 	{
-		AndroidApp app;
+		iOSApp app;
 
 		[SetUp]
 		public void BeforeEachTest ()
 		{
-			app = ConfigureApp.Android.StartApp ();
+			app = ConfigureApp.iOS.StartApp ();
 		}
 
 		[Test]
-		public void ClickingButtonTwiceShouldChangeItsLabel ()
+		public void ViewIsDisplayed ()
 		{
-			Func<AppQuery, AppQuery> MyButton = c => c.Button ("myButton");
+			AppResult[] results = app.WaitForElement (c => c.Child ("UIView"));
+			app.Screenshot ("First screen.");
 
-			app.Tap (MyButton);
-			app.Tap (MyButton);
-			AppResult[] results = app.Query (MyButton);
-			app.Screenshot ("Button clicked twice.");
-
-			Assert.AreEqual ("2 clicks!", results [0].Text);
+			Assert.IsTrue (results.Any ());
 		}
 	}
 }
+
 
