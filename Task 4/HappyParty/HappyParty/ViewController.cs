9311a22c68fe63b11c8c4ed6e44e99baa9eeb200
Святослав Щавровский <shchaurouski.slava@iMac-slawiko.local>
2015-10-11 04:58:08 +0300

    T-4: Core views added

--- Task 4/HappyParty/HappyParty/ViewController.cs
+++ Task 4/HappyParty/HappyParty/ViewController.cs
@@ -14,6 +14,19 @@ namespace HappyParty
 		{
 			base.ViewDidLoad ();
 			// Perform any additional setup after loading the view, typically from a nib.
+
+			nfloat scrollViewHeight = 100;
+
+			UIImageView imageView = new UIImageView{
+				Frame = new CoreGraphics.CGRect(View.Bounds.Top, View.Bounds.Top, View.Bounds.Width, View.Bounds.Height - scrollViewHeight)
+			};
+
+			UIScrollView scrollView = new UIScrollView {
+				Frame = new CoreGraphics.CGRect (View.Bounds.Top, View.Bounds.Bottom - scrollViewHeight, View.Bounds.Width, scrollViewHeight)
+			};
+
+			View.AddSubview (scrollView);
+			View.AddSubview (imageView);
 		}
 
 		public override void DidReceiveMemoryWarning ()
