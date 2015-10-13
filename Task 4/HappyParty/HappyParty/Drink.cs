5d678d162302ff1f37bf25d95b2db1e639ada7b6
slawiko <shchaurouski.slava@gmail.com>
2015-10-13 17:07:04 +0300

    T-4: Drink class added

---
+++ Task 4/HappyParty/HappyParty/Drink.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HappyParty
+{
+	public class Drink
+	{
+		public Drink (string name, nint amount)
+		{
+			this.name = name;
+			this.amount = amount;
+		}
+
+		string name {
+			get;
+			set;
+		}
+
+		nint amount {
+			get;
+			set;
+		}
+	}
+}
