d3e7cdc3af43ec1deb91e1f3d2e166d4c6a4e987
slawiko <shchaurouski.slava@gmail.com>
2015-10-20 22:46:33 +0300

    T-5_6: confirmation email sending support

--- Task_5_6/Task_5_6/settings.py
+++ Task_5_6/Task_5_6/settings.py
@@ -126,3 +126,26 @@ ACCOUNT_ACTIVATION_DAYS = 7
 REGISTRATION_AUTO_LOGIN = True
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
+
+# Console
+# EMAIL_HOST = 'localhost'
+# EMAIL_PORT = 1023
+# EMAIL_HOST_USER = 'user'
+# EMAIL_HOST_PASSWORD = 'password'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Sendgrid
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_HOST_USER = 'sendgrid_username'
+# EMAIL_HOST_PASSWORD = 'sendgrid_password'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+
+# Yandex
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = ''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- Task_5_6/app/views.py
+++ Task_5_6/app/views.py
@@ -2,21 +2,13 @@ from django.shortcuts import render
 
 
 def home(request):
-	if request.user.is_authenticated():
-		user = request.user
-	else:
-		user = ''
-
 	context = {
-		'user': user
 	}
 	return render(request, 'home.html', context)
 
 
 def about(request):
-	user = request.user
 	context = {
-		'user': user
 	}
 	return render(request, 'about.html', context)
 
